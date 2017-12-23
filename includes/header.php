610aac8dd1f27c568720ca795d46873495578f4b
dfringeli <dominik.fringeli@students.fhnw.ch>
2017-12-23 16:26:26 +0100

    Updated Google Chrome Theme Color on the settings. Implementent in the header.php

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <meta name="theme-color" content="#41d7f0">
+  <meta name="theme-color" content="<?php echo get_theme_mod('google_chrome_theme'); ?>">
 	<?php wp_head(); ?>
 </head>
 
